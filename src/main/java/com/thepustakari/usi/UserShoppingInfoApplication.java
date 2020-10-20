eb518833c7cc6161ddea9467bb85dcbde7da57b2
abc <abc@example.com>
2020-10-20 18:55:38 -0400

    first commit

---
+++ src/main/java/com/thepustakari/usi/UserShoppingInfoApplication.java
@@ -0,0 +1,14 @@
+package com.thepustakari.usi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserShoppingInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserShoppingInfoApplication.class, args);
+		System.out.println("welcome to spring boot application");
+	}
+
+}
---
+++ src/test/java/com/thepustakari/usi/UserShoppingInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepustakari.usi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserShoppingInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
